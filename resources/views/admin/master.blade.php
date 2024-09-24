2c95d61a27be808ebdfeeb88ab009af54fed5aff
Hồ Chí Hưng <hunghc89@gmail.com>
2024-09-24 09:50:12 +0700

    .

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="{{asset('dist/img/vlute_icon36.png')}}"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Quản lý lịch trình</title>
     @include('library-css')
